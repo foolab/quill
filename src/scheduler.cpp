29026a1764564d6b0740c7f1ab89a5efbcd3158e
Wenpeng Zhou <ext-wenpeng.2.zhou@nokia.com>
2011-09-13 10:02:20 +0300

    fixed the Bug 265971 - Delay in loading thumbnails if non jpeg image of size more than 16 megapixels is present at the start of grid

--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -472,6 +472,13 @@ Task *Scheduler::newNormalTask(File *file, int level)
     else if (prev == 0)
     {
         QSize fullSize = command->fullImageSize();
+        //We check if the image size exceeds image size limit
+        //if exceeds, we will not create a task at all.
+        //Even though we emitted the signal from quillundostack to ask gallery to destroy quillfile object, it is
+        //asynchronous process and probably the file to be removed still exists in the file list in core class.
+        /*if (!file->checkImageSize(fullSize)){
+            return 0;
+            }*/
         QSize targetSize = Core::instance()->targetSizeForLevel(level, fullSize);
         prevImage = QuillImage();
         prevImage.setFullImageSize(fullSize);
